4139f3989a78f8a0e243d11b9397478a2f9d5672
jihyuno301 <joh755807@gmail.com>
2018-12-20 17:01:47 +0900

    form validation2

--- src/main/java/net/skhu/model/BookModel.java
+++ src/main/java/net/skhu/model/BookModel.java
@@ -8,12 +8,12 @@ import lombok.Data;
 @Data
 public class BookModel {
 	
-	@NotEmpty
-	@Size(min=3, max=10)
+	@NotEmpty(message="책 제목을 입력하세요")
+	@Size(min=5, max=10)
 	String title;
 	
 	@NotEmpty
-	@Size(min=3, max=10)
+	@Size(min=3, max=10, message="저자의 이름을 3자 이상으로 적으세요")
 	String author;
 	
 	
